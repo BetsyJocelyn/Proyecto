9b728976e714de35e0b064755e48dea6d44c63c6
Diegomar02 <118679780+Diegomar02@users.noreply.github.com>
2022-12-09 08:48:58 -0600

    correos

--- correo_sus.php
+++ correo_sus.php
@@ -1,21 +1,21 @@
+
+
 <?php 
 error_reporting(-1);
 ini_set('display_errors', 'On');
 set_error_handler("var_dump");
 
-$subject ='Bienvenido a Salud Digna';
-$msg =  $_POST['msg'];
-$from1=$_POST['marquezuaa@gmail.com'];
+$subject ='Bienvenido a Pasteleria';
+$msg =  $_POST['Bienvenido a Pasteleria, a partir de hoy recibira descuentos exclusivos, por el momento le dejamos un 20% en cualquier compra con el cupon "BIENVENIDO22"'];
 $from = $_POST['from'];
 
 
-
 $headers = "From: marquezuaa@gmail.com\r\n"; 
-$headers .= "Reply-To: $from1\r\n";
+$headers .= "Reply-To: $from\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=utf-8\r\n"; 
 	
-if(mail($from1,$subject,$msg,$from,$headers)){
+if(mail($from,$subject,$msg,$headers)){
 	include ('index.html');
     
 	}else{
@@ -23,4 +23,3 @@ if(mail($from1,$subject,$msg,$from,$headers)){
 	echo $errorMessage;
 } 
 ?>
-
